feaabce5e4a99e4f3215143b6a497d89c62fb072
Julius Bacosa <juliusbacosa@gmail.com>
2020-05-04 09:39:07 +0800

    #1 : played with of

--- Rx-Web/src/app/modules/catalog/products/product-detail/product-detail.component.ts
+++ Rx-Web/src/app/modules/catalog/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
+import { Product } from './../../../../model/product.model';
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,6 +12,27 @@ export class ProductDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.checkOf();
+  }
+
+  checkOf() {
+    const source = of('julis', 'vlad', 'meme');
+    source.subscribe(res => {
+      console.log(res);
+    });
+
+
+    const product = {
+      Id: 1,
+      Name: 'Of Product',
+      Price: 890
+    } as Product;
+
+    const src = of(product);
+    src.subscribe(res => {
+      console.log(res);
+    });
+
   }
 
 }
